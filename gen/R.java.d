0e04dbdf44ba67e71ccfba887ca26900673b9536
Veljko Bogosavljevic <veljko.bogosavljevic@gmail.com>
2014-03-27 08:45:31 +0100

    Initial commit

---
+++ gen/R.java.d
@@ -0,0 +1,12 @@
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\gen\com\crypter\R.java \
+ : C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\anim\splash_rotation.xml \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable\splash_background.xml \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable-hdpi\splash_circle.png \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable-hdpi\splash_droid.png \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable-hdpi\splash_hands.png \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\layout\splashscreen.xml \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\res\values\strings.xml \
+C:\Users\Veljko\Documents\Leinengen\android-leiningen\crypter\AndroidManifest.xml \
