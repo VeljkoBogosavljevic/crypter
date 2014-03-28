c2be73586c1272de8deb2bb776aa7b87aacb5dfc
Veljko Bogosavljevic <veljko.bogosavljevic@gmail.com>
2014-03-28 13:06:33 +0100

    Login form, buttons, some functionalities

--- src/clojure/com/crypter/main.clj
+++ src/clojure/com/crypter/main.clj
@@ -1,7 +1,30 @@
 (ns com.crypter.main
   (:use [neko.activity :only [defactivity set-content-view!]]
         [neko.threading :only [on-ui]]
-        [neko.ui :only [make-ui]]))
+        [neko.ui :only [make-ui]]
+        [neko.notify])
+  (:import android.widget.EditText
+           android.content.Intent
+           android.widget.Toast))
+
+(declare exit-application login-notification)
+
+
+(def main-layout [:linear-layout {:orientation :vertical
+                                  :gravity :center
+                                  :id-holder true}
+                  [:edit-text {:hint "Username"
+                               :id ::username}]
+                  [:edit-text {:hint "Password"
+                               :id ::password}]
+                  [:linear-layout {:orientation :horizontal
+                                  :gravity :center
+                                  :id-holder true}
+                   [:button {:text "Login"
+                             :on-click (fn [_] (login-notification))}]
+                   [:button {:text "Exit"
+                             :on-click (fn [_] (exit-application))}]]])
+
 
 (defactivity com.crypter.MainActivity
   :def a
@@ -9,7 +32,19 @@
   (fn [this bundle]
     (on-ui
      (set-content-view! a
-      (make-ui [:linear-layout {:orientation :vertical
-                                :gravity :center}
-                [:text-view {:text "Crypter Application"}]
-                [:text-view {:text "by Veljko Bogosavljevic"}]])))))
+      (make-ui main-layout)))))
+
+(defn exit-application []
+  (.finish a))
+
+
+ (defn login-notification []
+  (fire :mynotification
+      (notification :icon com.crypter.R$drawable/ic_launcher
+                    :ticker-text "Crypter - Login successful !"
+                    :content-title "Crypter"
+                    :content-text "Login successful !"
+                    :action [:activity Intent/CATEGORY_HOME])))
+
+; (exit-application)
+; (.startActivity a (.getIntent a))
